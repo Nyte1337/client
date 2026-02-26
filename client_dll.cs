4851ee22a3243ab6d30ff07e16ea5f39ad452f60
Nyte1337 <81282735+Nyte1337@users.noreply.github.com>
2026-02-26 06:14:45 +0300

    Update client_dll.cs

--- client_dll.cs
+++ client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 03:16:32.813967100 UTC
+// 2026-02-26 03:13:52.527468800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
